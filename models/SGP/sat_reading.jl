5f1264a5dfd4e0f11344dd53e2efa4d64219c058
Benoit <benoitloger@gmail.com>
2019-12-11 13:48:11 +0100

    few corrections

--- models/SGP/sat_reading.jl
+++ models/SGP/sat_reading.jl
@@ -9,17 +9,22 @@
 
 const p, g, w = 4,4,4
 
-function read_sat_results()
+"""
+    read_sat_results(file_name)
+
+Read minisat results from "file_name" and print the corresponding SGP
+solution in the REPL
+"""
+function read_sat_results(file_name::String)
     q = p*g
     nb_var = q*p*g*w
-    ss = Vector{Int}(undef,nb_var+1)
-	open("/home/benoit/Bureau/test.out") do f
-		s = readlines(f)
-		println(s[1])
-		if s[1] == "SAT" || s[1] == "sat"
-			s = split(s[2]," ")
-			for i in 1:size(s,1)
-				ss[i]=parse(Int,s[i])
+    results = Vector{Int}(undef,nb_var+1)
+	open(file_name) do f
+		lines = readlines(f)
+		if lines[1] == "SAT" || lines[1] == "sat"
+			lines = split(lines[2]," ")
+			for i in 1:size(lines,1)
+				results[i]=parse(Int,lines[i])
 			end
         end
     end
@@ -31,8 +36,8 @@ function read_sat_results()
             for position in 1:p
                 for player in 1:q
                     var = (week-1)*(q*p*g) + (group-1)*(q*p) + (position-1)*(q) + player
-                    if ss[var] > 0
-                        print(Int(int_to_var(ss[var])[4]) ," ")
+                    if results[var] > 0
+                        print(Int(int_to_var(results[var])[4]) ," ")
                     end
                 end
             end
@@ -51,4 +56,4 @@ function int_to_var(value::Int)
     return w_var, g_var, p_var, q_var
 end
 
-read_sat_results()
+read_sat_results("/home/benoit/Bureau/test.out")
